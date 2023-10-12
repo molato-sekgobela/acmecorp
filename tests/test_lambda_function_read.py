d1a1f4c0576d1ca9572d9af0e6dc41ce076ec7ac
Molato Sekgobela <molato.sekgobela@gmail.com>
2023-10-12 22:41:55 +0200

    adding unittests

---
+++ tests/test_lambda_function_read.py
@@ -0,0 +1,63 @@
+# tests/test_lambda_function_read.py
+
+import unittest
+from unittest.mock import patch, Mock
+import lambda_function_read as read
+from unittest import TestCase, mock
+from lambda_function_read import lambda_handler
+import json
+import os
+
+class TestLambdaFunctionRead(unittest.TestCase):
+    
+    @mock.patch("lambda_function_read.mysql.connector.connect")
+    @mock.patch("lambda_function_read.s3_client.get_object")
+    @mock.patch.dict(os.environ, {
+    'host': 'mocked_host',
+    'database': 'mocked_database',
+    'user': 'mocked_user',
+    'password': 'mocked_password'
+})
+    def test_lambda_handler_success(self, mock_get_object, mock_db_connect):
+        
+        mock_body_content = "device_id,timestamp,temperature,humidity,hvac_status\n1,2022-10-12 00:00:00,25,50,on"
+        mock_get_object.return_value = {
+            "Body": mock.MagicMock(read=lambda: mock_body_content.encode('utf-8'))
+        }
+
+
+        # Set up the mock for the mysql connection and cursor
+        mock_connection = mock_db_connect.return_value
+        mock_cursor = mock_connection.cursor.return_value
+        mock_cursor.rowcount = 1
+        
+        # Prepare the event for testing
+        event = {
+            "Records": [{
+                "s3": {
+                    "bucket": {"name": "test_bucket"},
+                    "object": {"key": "test_key.txt"}
+                }
+            }]
+        }
+        
+        # Call the lambda_handler function
+        response = lambda_handler(event, {})
+        
+        # Validate the response
+        self.assertEqual(response["statusCode"], 200)
+        self.assertEqual(response["body"], json.dumps('success!'))
+
+
+
+    @patch("lambda_function_read.boto3.client")
+    def test_lambda_handler_no_records(self, mock_s3):
+        event = {}
+        response = read.lambda_handler(event, None)
+
+        self.assertEqual(response["statusCode"], 400)
+        self.assertEqual(response["body"], '"Event does not contain Records."')
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/test_lambda_function_retrieve.py
@@ -0,0 +1,52 @@
+# tests/test_lambda_function_retrieve.py
+
+import unittest
+from unittest.mock import patch, Mock, MagicMock
+import lambda_function_retrieve as retrieve_func
+import json
+
+
+class TestLambdaFunctionRetrieve(unittest.TestCase):
+
+    @patch("mysql.connector.connect")
+    @patch.dict("os.environ", {
+        "host": "localhost",
+        "database": "test_db",
+        "user": "test_user",
+        "password": "test_password"
+    })
+    def test_lambda_handler_success(self, mock_connect):
+        # Mocking MySQL connection and cursor
+        mock_cursor = Mock()
+        mock_cursor.fetchall.return_value = [
+            {
+                "device_id": "device_001",
+                "timestamp": "2022-10-12 12:00:00",
+                "temperature": 25.0,
+                "humidity": 50,
+                "hvac_status": "on"
+            }
+        ]
+        mock_connect.return_value = MagicMock(cursor=Mock(return_value=mock_cursor))
+
+        # Test event
+        event = {
+            "device_id": "device_001",
+            "date_range": {
+                "start": "2022-10-01",
+                "end": "2022-10-30"
+            }
+        }
+
+        response = retrieve_func.lambda_handler(event, None)
+        self.assertEqual(response["statusCode"], 200)
+        self.assertIn("Data", json.loads(response["body"]))
+        self.assertIn("Temperature Statistics", json.loads(response["body"]))
+        self.assertIn("Humidity Statistics", json.loads(response["body"]))
+        mock_cursor.execute.assert_called()
+
+    # Again, you can add more tests for various scenarios.
+
+
+if __name__ == "__main__":
+    unittest.main()
