ebfdbb0b52c7e07053030aa0214fa6e3b13db437
Molato Sekgobela <molato.sekgobela@gmail.com>
2023-10-24 11:16:30 +0200

    run tests

--- read_from_rds/lambda_function.py
+++ read_from_rds/lambda_function.py
@@ -38,7 +38,7 @@ def retrieve_data(device_id, start_date, end_date):
     cursor = connection.cursor(dictionary=True)
 
     # Execute query
-    query = ("SELECT device_id,temperature,humidity FROM iot_device_data "
+    query = ("SELECT device_id,timestamp,temperature,humidity FROM iot_device_data "
              "WHERE device_id = %s AND timestamp BETWEEN %s AND %s")
     cursor.execute(query, (device_id, start_date, end_date))
 
